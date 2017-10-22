86c7e60280a09af6329782b03819cc53fe64466f
liuxue0524 <liuxue0524@duia.com>
2017-10-22 17:14:16 +0800

    初始提交

--- src/main/java/com/alice/rabbitmq/_02_workqueue/NewTask.java
+++ src/main/java/com/alice/rabbitmq/_02_workqueue/NewTask.java
@@ -5,28 +5,34 @@ import java.io.IOException;
 import com.rabbitmq.client.Channel;  
 import com.rabbitmq.client.Connection;  
 import com.rabbitmq.client.ConnectionFactory;  
-  
+
+/**
+ * 通过一个工作队列（queue）分发耗时任务给不同的工作者(consumers)。
+ * 
+ * @author admin
+ *
+ */
 public class NewTask {
     //队列名称  
-    private final static String QUEUE_NAME = "workqueue";  
+    private final static String QUEUE_NAME = "workqueue";
 
     public static void main(String[] args) throws IOException {
-        //创建连接和频道 
+        //创建连接和频道
         ConnectionFactory factory = new ConnectionFactory();  
-        factory.setHost("localhost");  
+        factory.setHost("localhost");
         Connection connection = factory.newConnection();  
         Channel channel = connection.createChannel();  
-        //声明队列  
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);  
-        //发送10条消息，依次在消息后面附加1-10个点  
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        //发送10条消息，依次在消息后面附加1-10个点
         for (int i = 0; i < 10; i++) {  
-            String dots = "";  
-            for (int j = 0; j <= i; j++) {  
+            String dots = "";
+            for (int j = 0; j <= i; j++) {
                 dots += ".";  
             }  
-            String message = "helloworld" + dots+dots.length();  
+            String message = "helloworld" + dots+dots.length();
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes());  
-            System.out.println(" [x] Sent '" + message + "'");  
+            System.out.println(" [x] Sent '" + message + "'");
         }  
         //关闭频道和资源  
         channel.close();  
--- src/main/java/com/alice/rabbitmq/_02_workqueue/Work.java
+++ src/main/java/com/alice/rabbitmq/_02_workqueue/Work.java
@@ -5,11 +5,11 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;  
 import com.rabbitmq.client.QueueingConsumer;  
   
-public class Work { 
+public class Work {
     //队列名称  
     private final static String QUEUE_NAME = "workqueue";  
   
-    public static void main(String[] argv) throws java.io.IOException,  
+    public static void main(String[] args) throws java.io.IOException,  
             java.lang.InterruptedException {
         //区分不同工作进程的输出  
         int hashCode = Work.class.hashCode();  
@@ -25,28 +25,27 @@ public class Work {
       
         QueueingConsumer consumer = new QueueingConsumer(channel);  
         // 指定消费队列  
-        channel.basicConsume(QUEUE_NAME, true, consumer);  
-        while (true) {  
-            QueueingConsumer.Delivery delivery = consumer.nextDelivery();  
-            String message = new String(delivery.getBody());  
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = new String(delivery.getBody());
   
-            System.out.println(hashCode + " [x] Received '" + message + "'");  
-            doWork(message);  
-            System.out.println(hashCode + " [x] Done");  
+            System.out.println(hashCode + " [x] Received '" + message + "'");
+            doWork(message);
+            System.out.println(hashCode + " [x] Done");
   
-        }  
+        }
+    }
   
-    }  
-  
-    /** 
-     * 每个点耗时1s 
-     * @param task 
-     * @throws InterruptedException 
-     */  
-    private static void doWork(String task) throws InterruptedException {  
-        for (char ch : task.toCharArray()) {  
-            if (ch == '.') 
-                Thread.sleep(1000);  
-        }  
-    }  
-}  
+    /**
+     * 每个点耗时1s
+     * @param task
+     * @throws InterruptedException
+     */
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.')
+                Thread.sleep(1000);
+        }
+    }
+}
